7bfce416eaa84c0e9204379a42331a9e2a7c08b8
carpediemm85 <rustem.abbaszade@gmail.com>
2022-12-23 05:29:01 +0400

    image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import Nav from '../Nav/Nav'
+import Nav from './Nav/Nav'
+import Image from './images/Image'
+
 export default function Main() {
   return (
     <div>
       <Nav/>
+      <Image/>
     </div>
   )
 }
---
+++ src/components/images/Image.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Group1 from './Group1.png'
+import './image.css'
+export default function Image() {
+  return (
+    <div className='image'>
+      <img src={Group1} alt='applewatch' className='image'/>
+    </div>
+  )
+}
